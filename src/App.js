f16662576975de3e2413b2a20582da6ea7e832b1
Daniel Markow <daniel.markow@outlook.com>
2022-08-31 20:35:12 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [amount, setAmount] = useState(0);
+    const [balance, setBalance] = useState(0);
+    const [action, setAction] = useState("deposit");
+
+    function handleChange(event) {
+        let value = event.target.value;
+        setAmount(value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        switch (action) {
+            case "deposit":
+                setBalance(Number(balance) + Number(amount));
+                break;
+            case "cashback":
+                if (Number(amount) > Number(balance)) {
+                    alert("you can't overdraw");
+                    return;
+                }
+                setBalance(Number(balance) - Number(amount));
+                break;
+        }
+    }
+
+    return (
+        <>
+            <h1>ATM Example</h1>
+            <div>
+                <p>Action: {action === "deposit" ? "Deposit" : "Cash Back"}</p>
+                <button
+                    onClick={() => setAction("deposit")}
+                >Deposit</button>
+                <button
+                    onClick={() => setAction("cashback")}
+                >Cash Back</button>
+            </div>
+            <hr/>
+            <label>
+                Deposit:
+                <input type="number"
+                       min="0"
+                       value={amount}
+                       onChange={handleChange}
+                />
+                <button onClick={handleSubmit}>Submit</button>
+                <h2>Account Balance $ {balance}</h2>
+            </label>
+        </>
+      );
 }
 
 export default App;
